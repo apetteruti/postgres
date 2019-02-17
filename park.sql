9e208b15500073a9331e540e528f90ca9c4d9c65
ALP <alpetteruti@gmail.com>
2019-02-17 08:36:31 -0500

    read works, but not pretty

---
+++ park.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE park(
+    id INT NOT NULL,
+    parks_name VARCHAR(100),
+    location VARCHAR(100),
+    date_est INT, 
+    size VARCHAR (100),
+    visitors INT, 
+    description TEXT,
+    visited BOOLEAN,
+    PRIMARY KEY(id)
+);
--- postgres.js
+++ postgres.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg')
-
-
+var inquirer = require("inquirer");
+var Table = require('easy-table')
 
 const client = new Client({
     user: 'postgres',
@@ -11,10 +11,52 @@ const client = new Client({
   })
 
 
-
 client.connect()
 
-client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+client.query('SELECT $1::text as message', ['-----WELCOME TO The National Parks Tracker-----'], (err, res) => {
   console.log(err ? err.stack : res.rows[0].message) // Hello World!
-  client.end()
+  optionsMenu();
+  // client.end()
 })
+
+function readParks(){
+
+  // console.log("-----WELCOME TO The National Parks Tracker-----")
+
+  client.query("SELECT parks_name as parks_name, location as location, visited as visited FROM public.parks", 
+  function (err, results) {
+    if (err) throw err;
+    console.log(Table.print(results.rows));
+    optionsMenu();
+  });
+}
+
+
+
+
+
+var optionsMenu = function () {
+  inquirer.prompt([{
+
+    name: "options",
+    type: "list",
+    choices: ["See park list", "Create a new park", "Update a park", "Delete a park"],
+    message: "What would you like to do?"
+
+  }]).then(function (answer) {
+    switch (answer.options) {
+      case "See park list":
+        readParks();
+        break;
+      case "Create a new park":
+        createPark();
+        break;
+      case "Delete a park":
+        deletePark();
+        break;
+      case "Update a park":
+        updatePark();
+        break;
+    }
+  })
+}
